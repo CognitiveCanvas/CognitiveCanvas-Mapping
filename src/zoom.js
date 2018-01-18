62ab85dae3df9e9c0f547aabd865db249a7e2f2f
Gaurav Parmar <gparmar@ucsd.edu>
2018-01-18 15:48:48 -0800

    panning doesn't create a new node

--- src/zoom.js
+++ src/zoom.js
@@ -1,11 +1,13 @@
-/*var panning = 1;
+var panning = 1;
 
-var svg = d3.select(".canvas");
-  	.call(d3.behavior.zoom()
-  	.on("zoom", function () {
-      	svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
-      	panning = panning+1;
-  	})
-    .on("zoomstart", function() {
-      panning=0;
-    }))*/
+var svg = d3.select(".canvas")
+	.call(d3.behavior.zoom()
+		.on("zoom", ()=>{
+			svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+			panning = panning + 1;
+			console.log(panning);
+		})
+		.on("zoomstart", ()=>{
+			panning = 0;
+			console.log(panning);
+		}))
