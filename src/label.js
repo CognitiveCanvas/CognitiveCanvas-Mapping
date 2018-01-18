ec402d6929d0c686dead237fb2b536722a0d5218
Gaurav Parmar <gparmar@ucsd.edu>
2018-01-17 23:43:12 -0800

    Added compatibility for the case where label is edited for the second time

--- src/label.js
+++ src/label.js
@@ -1,62 +1,16 @@
 var temp_div = null;
-/*
- * Adds an editable label outside the svg 
- * that gets deleted once enter key is pressed
- *
- * Copies the text into svg g tag 
- * as a non-editable text element. 
- */
-
-/*
-var temp_div = null 
-
-    var addLabel = function(text, target){
-        var cx = (d3.select(target).attr("cx"))
-        var cy = (d3.select(target).attr("cy"))
-        //console.log(cx)
-        var svg = d3.select("#canvas") 
-        var g = svg.selectAll("g") 
-        var y = parseInt(cy)+15 
-
-        d3.select(target.parentNode).append("text")
-                .attr("x", cx)
-                .attr("y", y)
-                .text(text)
-                .classed("blackText", true)
-                .attr("id", d3.select(target.parentNode).attr("id")+"_text")
-
-        // Add an outside sibling div that is editable
-        temp_div = d3.select("body").append("div") 
-        console.log(temp_div)
-        temp_div.attr("contenteditable", "true") 
-        temp_div.attr("position", "absolute") 
-        temp_div.attr("z-index", 10) 
-        temp_div.attr("idx", d3.select(target.parentNode).attr("id")+"_text")
-        temp_div.style({
-            "left":cx+"px",
-            "top": cy+"px",
-            "position": "absolute"
-        }) 
-        temp_div.text(text) 
-        temp_div.on("keydown", ()=>{removeTemp()}) 
-        d3.select("body").append(temp_div) 
-    }
-    var editing = true 
-
-
-    function removeTemp (){
-        if (d3.event.keyCode == 13){
-            var temp_text = temp_div.text() 
-            var inside_text_id = temp_div.attr("idx") 
-            temp_div.remove() 
-            d3.select("#"+inside_text_id).text(temp_text) 
-        }
-    }
-*/
-
 
 function addLabels(text, target){
-  console.log('adding label')
+  console.log('adding label');
+  var num_labels = d3.select(target.parentNode).selectAll(".label").size();
+  // If a label already exists
+  if (num_labels > 0){
+    // Update the text 
+    text = d3.select(target.parentNode).select(".label").text();
+    // Delete the inside node
+    d3.select(target.parentNode).select(".label").remove();
+  }
+
   // Adding an editable div outside
   var container = document.getElementById("d3_container");
   var label = document.createElement("div");
@@ -70,22 +24,25 @@ function addLabels(text, target){
   label.style.left = cx + "px";
   label.style.top = cy + "px";
   label.setAttribute("contenteditable", "true");
+
   label.onkeypress = function(e){
   	if(e.key == 'Enter'){
   		var txt = label.textContent;
-  		var new_label = document.createElement("text");
-  		new_label.innerHTML = text;
+  		//var new_label = document.createElement("text");
+  		//new_label.innerHTML = text;
 
   		d3.select(target.parentNode).append("text")
                 .attr("x", "-10px")
                 .attr("y", "-10px")
-                .text(text)
+                .text(txt)
+                .classed("label", true)
                 .classed("blackText", true)
                 .attr("id", d3.select(target.parentNode).attr("id")+"_text")
-  		target.parentNode.appendChild(new_label);
+  		//target.parentNode.appendChild(new_label);
   		label.remove();
   	}
   }
   container.appendChild(label)
   
 }
+
