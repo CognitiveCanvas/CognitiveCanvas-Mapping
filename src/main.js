dc8f2966a7abc3986433c28b2b22b998b7dee644
Isaac Fehr <ifehr@ucsd.edu>
2018-01-23 10:44:00 -0800

    Fixed various bugs in grouping.  Groups now move together, can be created in an direction, and can have nodes added after creation

--- src/main.js
+++ src/main.js
@@ -36,10 +36,6 @@ function mouseUpListener(e) {
       console.log("single click");
       singleClickEvent(e);
     }, delay);
-
-    if (mouseMoved) {
-      resetState();
-    }
   }
   else if (mouseUp === 2 && mouseDown === 2 && !mouseMoved) {
     console.log("double click");
@@ -107,17 +103,20 @@ function mouseMoveListener(e) {
   if (mouseDown > 0) {
     mouseMoved = true
   }
-
   if (mouseDown === 1 && mouseUp === 0) {
-    var selection = d3.select(e.target);
     if(dragged_object){
-      var selection = d3.select(dragged_object);
-      if(selection.classed("node")){
-        drawDragNode(e);
-      }else if(selection.classed("selection_area")){
-        moveGroup(selection.node(), e.pageX, e.pageY);
+      switch(dragged_object.getAttribute("class").split(" ")[0] ){
+        case "node":
+          drawDragNode(e);
+          break;
+        case "selection_area":
+          moveGroup(dragged_object, e.pageX, e.pageY);
+          break;
+        default:
+          break;
       }
-    }else if(selection.classed("canvas")){
+    }else{
+      console.log("drawing selection area");
       drawSelectionArea(e);
     }
   }
@@ -133,21 +132,38 @@ function mouseMoveListener(e) {
  * - existing link: 
  */
 function singleClickEvent(e) {
+  if(mouseUp != 1 || mouseDown != 1){
+    return;
+  }
   let entity = e.target.getAttribute("class").split(" ")[0];
 
   if(selection_area){
+    console.log("single click while there is a selection area");
     createGroup();
-  } else{
+  } else if(dragged_object){
+    console.log("single click while there is a dragged object");
+  } else {
+    console.log("regular single click");
     switch(entity) {
       case "canvas":
-        let addedNode = addNode();
+        var addedNode = addNode();
         drawNode(addedNode, e.clientX, e.clientY, radius);
         break;
       case "node":
         break;
       case "link":
         break;
-      default: 
+      case "selection_area":
+        var addedNode = addNode();
+        drawNode(addedNode, e.clientX, e.clientY, radius);
+        let new_children_ids = String(e.target.getAttribute("children_ids"))
+          .split(" ")
+          .filter(x => x);
+        new_children_ids.push(addedNode.id);
+        new_children_ids = new_children_ids.join(' ');
+        e.target.setAttribute("children_ids", new_children_ids);
+        break;
+      default:
         break;
     }
   }
@@ -268,6 +284,13 @@ function removeNode(node) {
   d3.selectAll(`[target_id=${node_id}]`)
     .remove();
 
+
+  d3.selectAll(".selection_area[children_ids~=" + node_id + "]")
+    .each(function(){
+      let group = d3.select(this);
+      group.attr("children_ids", group.attr("children_ids").split(' ').filter(id => id !== node_id).join(' ') );
+    });
+
   node_d3.remove();
 }
 
@@ -314,6 +337,7 @@ function addLabel(text, cx, cy) {
 }
 
 function resetState() {
+  console.log("state was reset");
   mouseDown = 0;
   mouseUp = 0;
   mouseMoved = false;
@@ -385,8 +409,8 @@ function drawDragNode(e) {
 }
 
 function drawSelectionArea(e){
-    //console.log("Drawing Selection Area");
     if (!selection_area){
+      console.log("Creating Selection Area");
         selection_area = d3.select(canvas).insert("rect", ":first-child")
             .classed("selection_area", true)
             .attr("x", e.pageX)
@@ -395,31 +419,46 @@ function drawSelectionArea(e){
             .attr("height", 0)
         //console.log("selection is created");
     }
-    selection_area
-        .attr("width", e.pageX - selection_area.attr("x"))
-        .attr("height", e.pageY - selection_area.attr("y"));
+    var rectX = Number(selection_area.attr("x"));
+    var rectY = Number(selection_area.attr("y"));
+    var width = Number(selection_area.attr("width"));
+    var height = Number(selection_area.attr("height"));
+    var midX = rectX + width / 2;
+    var midY = rectY + height / 2;
+
+    if( e.pageX > midX ){
+      selection_area.attr("width", e.pageX - rectX);
+    } else{
+      selection_area.attr("width", rectX - e.pageX + width);
+      selection_area.attr("x", e.pageX);
+    }
+    if( e.pageY > midY ){
+      selection_area.attr("height", e.pageY - rectY);
+    } else{
+      selection_area.attr("height", rectY - e.pageY + height);
+      selection_area.attr("y", e.pageY);
+    }
     //console.log("selection area drawn");
 }
     
 function createGroup(){
   console.log("creating group");
-  var left = selection_area.attr("x");
-  var right = left + selection_area.attr("width");
-  var top = selection_area.attr("y");
-  var bottom = top + selection_area.attr("height");
+  var left = Number(selection_area.attr("x"));
+  var right = left + Number(selection_area.attr("width"));
+  var top = Number(selection_area.attr("y"));
+  var bottom = top + Number(selection_area.attr("height"));
 
-  var grouped_nodes = d3.selectAll(".node")
+  var allNodes = d3.selectAll(".node")
   var children_ids = [];
-  console.log(grouped_nodes)
-  grouped_nodes.filter( function() {
-      var position = getNodePosition(this);
-      var x = position[0];
-      var y = position[1];
-      return x >= left
-          && x <= right
-          && y >= top 
-          && y <= bottom;
-      });
+  console.log(grouped_nodes);
+  console.log("left: ", left, ", right: ", right, ", top: ", top, ", bottom: ", bottom);
+  var grouped_nodes = allNodes.filter( function() {
+    var position = getNodePosition(this);
+    var x = position[0];
+    var y = position[1];
+    console.log("x: ", x, ", y: ", y);
+    return x >= left && x <= right && y >= top && y <= bottom;
+  });
   console.log(grouped_nodes)
   grouped_nodes.each(function(){children_ids.push(d3.select(this).attr("id"))});
   selection_area.attr("children_ids", children_ids.join(" "));
@@ -430,7 +469,7 @@ function createGroup(){
 
 function moveGroup(group, x, y){
   var group = d3.select(group);
-  var nodeIds = group.attr("children_ids").split(" ");
+  var nodeIds = group.attr("children_ids").split(" ").filter(x => x);
 
   var xMove = x - group.attr("x") + drag_offset[0];
   var yMove = y - group.attr("y") + drag_offset[1];
