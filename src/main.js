b6df8331664956845fb4746edb20ea961c80a47f
Unknown <xiw180@ucsd.edu>
2018-01-24 21:08:02 -0800

    Revert "origin file add edge content stuff - Unfinished"
    This reverts commit a755933e6f8146da11982163a7372f05cf4611e8.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@
 var nodes = [];
 var links = [];
 
-var clientId, active_node, dragged_node = null;
+var clientId, active_node, dragged_object = null;
+var drag_offset = [0, 0];
 
 var canvas = document.getElementById("canvas");
 var radius = 20;
@@ -13,12 +14,15 @@ var mouseDown = 0;
 var delay = 300;
 var mouseMoved = false;
 var singleClickTimer, duobleClickDragTimer = null;
-var mouseHoverEle = false;
-var hoveredEle = null;
+var mouseHoverNode = false;
+var hoveredNode = null;
+var mouseHoverEdge = false;
+var hoveredEdge = null;
 
 // drag_line & source_node are stored as html element
 var drag_line = null;
 var source_node = null;
+var selection_area = null;
 var zoom = null;
 
 canvas.addEventListener("mouseup", (e) => mouseUpListener(e));
@@ -31,8 +35,6 @@ window.addEventListener("keypress", (e) => keyPressListener(e));
 
 function mouseUpListener(e) {
   mouseUp++;
-
-  console.log("mouse up", mouseUp);
 
   if (mouseUp === 1 && mouseDown === 1) {
     singleClickTimer = setTimeout(() => {
@@ -59,17 +61,17 @@ function mouseUpListener(e) {
 
 function mouseDownListener(e) {
   mouseDown++;
+  e.preventDefault();
 
-  console.log("mouse down", mouseDown);
   if (mouseDown === 1) {
-    selectDragNode(e);
+    selectDraggedObject(e);
   }
   else if (mouseDown === 2 && mouseUp === 1) {
     clearTimeout(singleClickTimer);
     selectSrcNode(e);
     doubleClickDragTimer = setTimeout(() => {
       let className = e.target.getAttribute("class").split(" ")[0];
-      if (className === "node") {
+      if (className === "node-rep") {
         revealDragLine();
       }
     }, delay);
@@ -85,15 +87,18 @@ function rightClickListener(e) {
   if (className != "canvas") {
     e.preventDefault();
   }
-  
+
   switch(className) {
-    case "node":
-      deleteEntity(nodes, e.target.getAttribute("id"));
-      removeNode(e.target);
+    case "node-rep":
+      deleteEntity(nodes, e.target.parentNode.getAttribute("id"));
+      removeNode(e.target.parentNode);
       break;
-    case "link":
-      deleteEntity(links, e.target.getAttribute("id"));
-      removeLink(e.target);
+    case "link-rep":
+      deleteEntity(links, e.target.parentNode.getAttribute("id"));
+      removeLink(e.target.parentNode);
+      break;
+    case "selection_area":
+      d3.select(e.target).remove();
       break;
     default:
       break;
@@ -101,15 +106,95 @@ function rightClickListener(e) {
   resetState();
 }
 
+
+function mouseOverListener(e) {
+  let className = e.target.getAttribute("class").split(" ")[0];
+  
+  switch(className) {
+    case "node":
+      mouseHoverNode = true;
+      hoveredNode = e.target.getAttribute("id");
+      if (nodes.find(x => x.id === hoveredNode)){
+          if (nodes.find(x => x.id === hoveredNode).content){
+          var addressToFrame = "http://webstrates.ucsd.edu/" + hoveredNode;
+              
+          var wrapper = document.createElement('div');
+          wrapper.setAttribute("id", "showing");
+
+          var toFrame = '<iframe src="'+ addressToFrame + '" style="position: absolute;left: 8px;top: 8px;width: 300px;height: 300px;"><p>ERROR: Your browser does not support iframes.</p></iframe>';
+              
+          wrapper.innerHTML = toFrame;
+              
+          document.body.appendChild(wrapper);
+        }
+      }
+      break;
+    case "link":
+      mouseHoverEdge = true;
+      break;
+    default:
+      break;
+  }
+}
+
+function mouseOutListener(e) {
+  let className = e.target.getAttribute("class").split(" ")[0];
+  
+  switch(className) {
+    case "node":
+      if (nodes.find(x => x.id === hoveredNode)) {  
+        if (nodes.find(x => x.id === hoveredNode).content){
+          var elem = document.getElementById("showing");
+          if (elem) {
+            elem.parentNode.removeChild(elem);
+          }
+        }
+      }
+                
+      mouseHoverNode = false;
+      hoveredNode = null;
+      break;
+    case "link":
+      mouseHoverEdge = false;
+      break;
+    default:
+      break;
+  }
+}
+
+function keyPressListener(e) {
+  var keyCode = e.which;
+    
+  if (keyCode == 97) {
+
+    if (mouseHoverNode){
+      addNodeContent(e);
+    }
+    else if (mouseHoverEdge){
+      addEdgeContent(e);
+    }
+    
+  }
+    
+}
+
 function mouseMoveListener(e) {
-  console.log("mouse moved");
   if (mouseDown > 0) {
     mouseMoved = true
   }
 
   if (mouseDown === 1 && mouseUp === 0) {
-    console.log("trigger drawDragNode")
-    drawDragNode(e);
+    var selection = d3.select(e.target);
+    if(dragged_object){
+      var selection = d3.select(dragged_object);
+      if(selection.classed("node-rep")){
+        drawDragNode(e);
+      }else if(selection.classed("selection_area")){
+        moveGroup(selection.node(), e.pageX, e.pageY);
+      }
+    }else if(selection.classed("canvas")){
+      drawSelectionArea(e);
+    }
   }
   else if (mouseDown === 2) {
     drawDragLine(e)
@@ -122,11 +207,11 @@ function mouseOverListener(e) {
   
   switch(className) {
     case "node":
-      mouseHoverEle = true;
-      hoveredEle = e.target.getAttribute("id");
-      if (nodes.find(x => x.id === hoveredEle)){
-          if (nodes.find(x => x.id === hoveredEle).content){
-          var addressToFrame = "http://webstrates.ucsd.edu/" + hoveredEle;
+      mouseHoverNode = true;
+      hoveredNode = e.target.getAttribute("id");
+      if (nodes.find(x => x.id === hoveredNode)){
+          if (nodes.find(x => x.id === hoveredNode).content){
+          var addressToFrame = "http://webstrates.ucsd.edu/" + hoveredNode;
               
           var wrapper = document.createElement('div');
           wrapper.setAttribute("id", "showing");
@@ -140,22 +225,7 @@ function mouseOverListener(e) {
       }
       break;
     case "link":
-      mouseHoverEle = true;
-      hoveredEle = e.target.getAttribute("id");
-      if (links.find(x => x.id === hoveredEle)){
-          if (links.find(x => x.id === hoveredEle).content){
-          var addressToFrame = "http://webstrates.ucsd.edu/" + hoveredEle;
-              
-          var wrapper = document.createElement('div');
-          wrapper.setAttribute("id", "showing");
-
-          var toFrame = '<iframe src="'+ addressToFrame + '" style="position: absolute;left: 8px;top: 8px;width: 300px;height: 300px;"><p>ERROR: Your browser does not support iframes.</p></iframe>';
-              
-          wrapper.innerHTML = toFrame;
-              
-          document.body.appendChild(wrapper);
-        }
-      }
+      mouseHoverEdge = true;
       break;
     default:
       break;
@@ -167,45 +237,42 @@ function mouseOutListener(e) {
   
   switch(className) {
     case "node":
-      if (nodes.find(x => x.id === hoveredEle)) {  
-        if (nodes.find(x => x.id === hoveredEle).content){
+      if (nodes.find(x => x.id === hoveredNode)) {  
+        if (nodes.find(x => x.id === hoveredNode).content){
           var elem = document.getElementById("showing");
           if (elem) {
             elem.parentNode.removeChild(elem);
           }
         }
       }
+                
+      mouseHoverNode = false;
+      hoveredNode = null;
       break;
     case "link":
-      if (links.find(x => x.id === hoveredEle)) {  
-        if (links.find(x => x.id === hoveredEle).content){
-          var elem = document.getElementById("showing");
-          if (elem) {
-            elem.parentNode.removeChild(elem);
-          }
-        }
-      }
+      mouseHoverEdge = false;
       break;
     default:
       break;
   }
-  mouseHoverEle = false;
-  hoveredEle = null;
 }
 
 function keyPressListener(e) {
   var keyCode = e.which;
-  console.log(e);
     
   if (keyCode == 97) {
 
-    if (mouseHoverEle){
-      addContent(e);
+    if (mouseHoverNode){
+      addNodeContent(e);
+    }
+    else if (mouseHoverEdge){
+      addEdgeContent(e);
     }
     
   }
     
 }
+
 
 /*
  * Single click interaction
@@ -216,17 +283,21 @@ function keyPressListener(e) {
 function singleClickEvent(e) {
   let entity = e.target.getAttribute("class").split(" ")[0];
 
-  switch(entity) {
-    case "canvas":
-      let addedNode = addNode();
-      drawNode(addedNode, e.clientX, e.clientY, radius);
-      break;
-    case "node":
-      break;
-    case "link":
-      break;
-    default: 
-      break;
+  if(selection_area){
+    createGroup();
+  } else{
+    switch(entity) {
+      case "canvas":
+        let addedNode = addNode();
+        drawNode(addedNode, e.clientX, e.clientY, radius);
+        break;
+      case "node":
+        break;
+      case "link":
+        break;
+      default: 
+        break;
+    }
   }
 
   resetState();
@@ -268,8 +339,6 @@ function doubleClickEvent(e) {
 }
 
 
-
-
 /* entity.js */
 function getID() {
   return clientId + "_" + Date.now();
@@ -303,12 +372,14 @@ function drawNode(node, cx, cy, radius) {
 
   d3.select(canvas)
     .append("g")
-    .attr("class", "node_group")
-    .append("circle")
     .attr("class", "node")
+    .attr("id", node.id)
+    .attr("transform", "translate("+x+","+y+")")
+    .append("circle")
+    .attr("class", "node-rep")
     .attr("r", radius)
-    .attr("cx", x)
-    .attr("cy", y)
+    .attr("cx", 0)
+    .attr("cy", 0)
     .attr("id", node.id)
     .attr("xmlns", "http://www.w3.org/2000/svg");
 }
@@ -317,23 +388,26 @@ function drawLink(link) {
   let linkSrcNode = document.getElementById(link.sourceId);
   let linkDestNode = document.getElementById(link.destId);
 
-  let x1 = linkSrcNode.getAttribute("cx");
-  let y1 = linkSrcNode.getAttribute("cy");
-  let x2 = linkDestNode.getAttribute("cx");
-  let y2 = linkDestNode.getAttribute("cy");
+  let x1 = getNodePosition(linkSrcNode)[0];
+  let y1 = getNodePosition(linkSrcNode)[1];
+  let x2 = getNodePosition(linkDestNode)[0];
+  let y2 = getNodePosition(linkDestNode)[1];
+
+  console.log("linkSrcNode", linkSrcNode)
+
 
   d3.select(canvas)
     .insert("g", ":first-child")
-    .attr("class", "link_group")
-    .append("line")
     .attr("class", "link")
+    .attr("id", link.id)
+    .attr("source_id", link.sourceId)
+    .attr("target_id", link.destId)
+    .append("line")
+    .attr("class", "link-rep")
     .attr("x1", x1)
     .attr("y1", y1)
     .attr("x2", x2)
     .attr("y2", y2)
-    .attr("id", link.id)
-    .attr("source_id", link.sourceId)
-    .attr("target_id", link.destId)
     .attr("xmlns", "http://www.w3.org/2000/svg");
 }
 
@@ -397,24 +471,29 @@ function resetState() {
   mouseUp = 0;
   mouseMoved = false;
   source_node = null;
-  dragged_node = null;
+  dragged_object = null;
+  drag_offset = [0,0];
 }
 
-function selectDragNode(e) {
+function selectDraggedObject(e) {
   var selection = d3.select(e.target)
-  if (selection.classed("node")) {
-    dragged_node = selection
+  if (selection.classed("node-rep")) {
+    dragged_object = e.target;
+  } else if(selection.classed("selection_area")){
+    dragged_object = e.target;
+    var dragged_group = d3.select(dragged_object);
+    drag_offset = [dragged_group.attr("x") - e.pageX, dragged_group.attr("y") - e.pageY]
   }
 }
 
 function selectLineDest(e) {
   hideDragLine();
-  if (dragged_node) {
-    dragged_node = null
+  if (dragged_object) {
+    dragged_object = null
   }
   var selection = d3.select(e.target)
   let sourceNode = d3.select(source_node);
-  if (sourceNode && selection.classed("node")) {
+  if (sourceNode && selection.classed("node-rep")) {
     let addedLink = addLink(sourceNode.attr("id"), selection.attr("id"));
     drawLink(addedLink);
     source_node = null;
@@ -437,47 +516,138 @@ function hideDragLine() {
 }
 
 function selectSrcNode(e) {
-  source_node = e.target;
+  source_node = e.target.parentNode;
 }
 
 function drawDragLine(e) {
-  let sourceNode = d3.select(source_node);
+  let sourceNode = source_node;
   let dragLine = d3.select(drag_line);
 
-  dragLine.attr("x1", sourceNode.attr("cx"))
-          .attr("y1", sourceNode.attr("cy"))
+  dragLine.attr("x1", getNodePosition(sourceNode)[0])
+          .attr("y1", getNodePosition(sourceNode)[1])
           .attr("x2", e.pageX)
           .attr("y2", e.pageY)
 }
 
 function drawDragNode(e) {
-  if (dragged_node != null) {
-    let selected_id = dragged_node.attr("id");
+  if (dragged_object != null) {
+    let selected_id = d3.select(dragged_object).attr("id");
 
-    d3.selectAll(`[source_id=${selected_id}]`)
-      .attr("x1", e.pageX)
-      .attr("y1", e.pageY);
-
-    d3.selectAll(`[target_id=${selected_id}]`)
-      .attr("x2", e.pageX)
-      .attr("y2", e.pageY);
-
-    dragged_node
-      .attr("cx", e.pageX)
-      .attr("cy", e.pageY);
+    translateNode(dragged_object, e.pageX, e.pageY);
   }
 }
 
-function addContent(e) {
-    var nodeId = hoveredEle;
+
+function addNodeContent(e) {
+    var nodeId = hoveredNode;
     
-    var newNodeAddress = "http://webstrates.ucsd.edu/" + hoveredEle;
+    var newNodeAddress = "http://webstrates.ucsd.edu/" + hoveredNode;
     
     var appendElement = '<div class="note" contenteditable="true" style="position: absolute;left: 8px;top: 8px;width: 200px;min-height: 200px;padding: 16px;box-shadow: 5px 5px 10px gray;background-color: rgb(255, 255, 150);font-size: 24pt;word-wrap: break-word;"></div>'
     
     // Don't know why the elements does not append. TODO
     var openWindow = window.open(newNodeAddress).document.body.innerHTML += appendElement;
     
-    nodes.find(x => x.id === hoveredEle).content = true;
+    nodes.find(x => x.id === hoveredNode).content = true;
     
+}
+
+function addEdgeContent(e) {
+    console.log("addEdgeContent Triggered!");
+}
+
+
+function drawSelectionArea(e){
+    //console.log("Drawing Selection Area");
+    if (!selection_area){
+        selection_area = d3.select(canvas).insert("rect", ":first-child")
+            .classed("selection_area", true)
+            .attr("x", e.pageX)
+            .attr("y", e.pageY)
+            .attr("width", 0)
+            .attr("height", 0)
+        //console.log("selection is created");
+    }
+    selection_area
+        .attr("width", e.pageX - selection_area.attr("x"))
+        .attr("height", e.pageY - selection_area.attr("y"));
+    //console.log("selection area drawn");
+}
+    
+function createGroup(){
+  console.log("creating group");
+  var left = selection_area.attr("x");
+  var right = left + selection_area.attr("width");
+  var top = selection_area.attr("y");
+  var bottom = top + selection_area.attr("height");
+
+  var grouped_nodes = d3.selectAll(".node")
+  var children_ids = [];
+  console.log(grouped_nodes)
+  grouped_nodes.filter( function() {
+      var position = getNodePosition(this);
+      var x = position[0];
+      var y = position[1];
+      return x >= left
+          && x <= right
+          && y >= top 
+          && y <= bottom;
+      });
+  console.log(grouped_nodes)
+  grouped_nodes.each(function(){children_ids.push(d3.select(this).attr("id"))});
+  selection_area.attr("children_ids", children_ids.join(" "));
+  
+  selection_area = null;
+  dragged_object = null;
+}
+
+function moveGroup(group, x, y){
+  var group = d3.select(group);
+  var nodeIds = group.attr("children_ids").split(" ");
+
+  var xMove = x - group.attr("x") + drag_offset[0];
+  var yMove = y - group.attr("y") + drag_offset[1];
+
+  for(i = 0; i < nodeIds.length; i++){
+    let nodeId = nodeIds[i];
+    var node = d3.select('#'+ nodeId);
+    translateNode(node.node(), xMove, yMove, true);
+  }
+
+  group.attr("x", x + drag_offset[0]);
+  group.attr("y", y + drag_offset[1]);
+}
+
+//TODO: Improve efficiency
+//Helper Function to move a node group
+/**
+node: the node to move along with its links
+x: the x coordinate to move the node to.  If relative is true, this will be an x offest instead 
+y: the y coordinate to move the node to.  If relative is true, this will be a y offest instead 
+**/
+function translateNode(node, x, y, relative=false){
+  //console.log("Node: ", node, ", X: ", x, ", Y: ", y);
+  if(relative){
+    var position = getNodePosition(node);
+    x += position[0];
+    y += position[1];
+  }
+
+  var g = d3.select(node.parentNode);
+  g.attr("transform", "translate(" + x + "," + y + ")");
+
+  let selected_id = d3.select(node).attr("id");
+
+  d3.selectAll(`[source_id=${selected_id}] > [class="link-rep"]`)
+    .attr("x1", x)
+    .attr("y1", y);
+
+  d3.selectAll(`[target_id=${selected_id}] > [class="link-rep"]`)
+    .attr("x2", x)
+    .attr("y2", y);
+
+}
+
+function getNodePosition(node){
+  return d3.transform(d3.select(node).attr("transform")).translate;
 }
