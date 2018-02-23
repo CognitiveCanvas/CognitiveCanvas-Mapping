16a36554e96df797378253b88e8942b33426db19
GaParmar <gparmar@ucsd.edu>
2018-02-23 09:39:54 -0800

    Adding placeholder buttons

---
+++ src/custom_layout.js
@@ -0,0 +1,11 @@
+function dropbtn_click(){
+	document.getElement("myDropdown").classList.toggle("show");
+	console.log("button conflicts");
+}
+
+
+window.onclick = function(event){
+	if(!event.target.matches('.dropbtn')) {
+		var dropdowns = 
+	}
+}
