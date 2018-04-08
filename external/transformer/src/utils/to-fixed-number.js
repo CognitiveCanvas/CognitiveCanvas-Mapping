7baf262e7da59239cc160aeb4c5d8f16b829c50e
Isaac Fehr <ifehr@ucsd.edu>
2018-04-08 13:31:43 -0700

    cleaning up

--- external/transformer/src/utils/to-fixed-number.js
+++
@@ -1,8 +0,0 @@
-if (!Number.prototype.toFixedNumber) {
-    /* jshint ignore:start */
-    Number.prototype.toFixedNumber = function (x, base) {
-        let pow = Math.pow(base || 10, x);
-        return +(Math.round(this * pow) / pow);
-    };
-    /* jshint ignore:end */
-}
