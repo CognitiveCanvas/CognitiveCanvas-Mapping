8373021833b499c17b6ad7a75465667d6ab3d84d
GaParmar <gparmar@ucsd.edu>
2018-05-06 11:47:25 -0700

    reformatted the use of getNodeGroups

--- src/undo-redo.js
+++ src/undo-redo.js
@@ -79,11 +79,11 @@ function undoStyle(data){
 function removeNodeFromGroup(node){
   	let node_d3 = node instanceof d3.selection ?  node : d3.select(node);
   	let node_id = node_d3.attr("id");
-  	d3.selectAll(".selection_area[children_ids~=" + node_id + "]")
-    	.each(function(){
-    	let group = d3.select(this);
-      	group.attr("children_ids", group.attr("children_ids").split(' ').filter(id => id !== node_id).join(' ') );
-    	});
+    let groups = getNodeGroups(node);
+    for (let i = 0; i<groups.length; i++){
+    	let g = groups[i];
+    	g.attr("children_ids", g.attr("children_ids").split(' ').filter(id => id !== node_id).join(' ') );
+    }
 }
 
 function getNodeGroups(node){
