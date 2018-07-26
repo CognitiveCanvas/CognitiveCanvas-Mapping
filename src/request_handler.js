b58f35113098b01eaeb1058a678ef94c653b629b
Xavier (Xinwei Wang) <xinweixavier@gmail.com>
2018-07-26 15:34:44 -0700

    Add Message Handler for Webstrate

---
+++ src/request_handler.js
@@ -0,0 +1,12 @@
+/* Handling Messages that have been post on the Webstrate */
+window.onmessage = function(e) {
+  
+  console.log("Start Handling Message");
+  
+  if (e.data.id == "search") {
+    console.log("Message Type: Search");
+    sendRelatedEleToContainer(e.data.query);
+  }
+  
+}
+
